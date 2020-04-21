ae184bc35920f8bbc5d4e162b1a3c18c8789b404
StarTux <ths.loy@googlemail.com>
2020-04-21 12:32:30 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Fly
+
+Manage fly and walk speed.
+
+## Compatibility
+
+Tested on **Paper 1.15.2**, should work on a wide range of Bukkit, Spigot or Paper versions.
+
+## Commands
+
+- `/flyspeed` Set or get fly speed
+- `/walkspeed` Set or get walk speed
+
+## Function
+
+Fly and walk speeds are reset to default when a player joins or leaves, and when the plugin loads or unloads.
---
+++ src/main/java/com/cavetale/fly/FlyPlugin.java
@@ -0,0 +1,90 @@
+package com.cavetale.fly;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FlyPlugin extends JavaPlugin implements Listener {
+    public void onEnable() {
+        this.getServer().getPluginManager().registerEvents(this, this);
+        for (Player player : getServer().getOnlinePlayers()) {
+            player.setFlySpeed(0.1f);
+            player.setWalkSpeed(0.2f);
+        }
+    }
+
+    public void onDisable() {
+        for (Player player : getServer().getOnlinePlayers()) {
+            player.setFlySpeed(0.1f);
+            player.setWalkSpeed(0.2f);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().setFlySpeed(0.1f);
+        event.getPlayer().setWalkSpeed(0.2f);
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        event.getPlayer().setFlySpeed(0.1f);
+        event.getPlayer().setWalkSpeed(0.2f);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command,
+                             String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Player expected");
+            return true;
+        }
+        Player player = (Player) sender;
+        if (args.length == 0) {
+            switch (label) {
+            case "flyspeed":
+                int flySpeed = (int) (player.getFlySpeed() * 10.0f);
+                player.sendMessage(ChatColor.AQUA + "Fly speed: " + flySpeed);
+                break;
+            case "walkspeed":
+                int walkSpeed = (int) (player.getWalkSpeed() * 10.0f);
+                player.sendMessage(ChatColor.GREEN + "Walk speed: " + walkSpeed);
+                break;
+            default:
+                throw new IllegalStateException("label=" + label);
+            }
+            return true;
+        }
+        if (args.length != 1) return false;
+        int speed;
+        try {
+            speed = Integer.parseInt(args[0]);
+        } catch (NumberFormatException nfe) {
+            speed = 0;
+        }
+        if (speed < 1 || speed > 10) {
+            sender.sendMessage(ChatColor.RED + "Invalid speed: " + args[0]);
+            return true;
+        }
+        float floatSpeed = (float) speed * 0.1f;
+        switch (label) {
+        case "flyspeed":
+            player.setFlySpeed(floatSpeed);
+            player.sendMessage(ChatColor.AQUA + "Fly speed set to " + speed);
+            break;
+        case "walkspeed":
+            player.setWalkSpeed(floatSpeed);
+            player.sendMessage(ChatColor.GREEN + "Walk speed set to " + speed);
+            break;
+        default:
+            throw new IllegalStateException("label=" + label);
+        }
+        return true;
+    }
+}
